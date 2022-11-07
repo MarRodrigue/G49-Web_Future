bcef234122f2fc1f222166f8c299effe436c1900
Oscar Arrieta Basilio <3157893185oscar@gmail.com>
2022-11-06 22:17:13 -0500

    segunda version version-loopback

--- Loopback/BackendPetsecure/src/controllers/index.ts
+++ Loopback/BackendPetsecure/src/controllers/index.ts
@@ -1,1 +1,3 @@
 export * from './ping.controller';
+export * from './mascota-persona.controller';
+export * from './persona-mascota.controller';
---
+++ Loopback/BackendPetsecure/src/models/mascota.model.ts
@@ -0,0 +1,49 @@
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {Persona} from './persona.model';
+
+@model()
+export class Mascota extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  chip: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  raza: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  suscripcion: string;
+
+  @belongsTo(() => Persona)
+  personaId: string;
+
+  constructor(data?: Partial<Mascota>) {
+    super(data);
+  }
+}
+
+export interface MascotaRelations {
+  // describe navigational properties here
+}
+
+export type MascotaWithRelations = Mascota & MascotaRelations;
